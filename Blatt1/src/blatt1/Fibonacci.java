1526e184a672fc122361a2b41277a9c1ce3939cd
fabalex7 <fabiwestphal@gmail.com>
2019-04-05 21:01:03 +0200

    Fibonacci.java und FibonacciPrint.java überschrieben

--- Blatt1/src/blatt1/Fibonacci.java
+++ Blatt1/src/blatt1/Fibonacci.java
@@ -2,19 +2,16 @@ package blatt1;
 
 public class Fibonacci{
 
-    //Attribute
-    private int n;
-    
 
-    //Methoden
-    public Fibonacci(int zahl){
-    	this.n = zahl;
-    }
-    
-    public int next(){
-    	if
-    	
-    	
-    	fib.next();
-    }
+  int firstNumber = 0;  //f(0)
+  int secondNumber = 1; //f(1)
+
+  public int next(){
+    int sum = firstNumber + secondNumber;
+    firstNumber = secondNumber;
+    secondNumber = sum;
+
+    return sum;
+  }
+
 }
--- Blatt1/src/blatt1/FibonacciPrint.java
+++ Blatt1/src/blatt1/FibonacciPrint.java
@@ -1,14 +1,30 @@
 package blatt1;
 public class FibonacciPrint{
 
-    public static void main(String[] args) {
-        //Zahl einlesen
+  public static void main(String[] args) {
 
-        //Zahl berechnen
+    //Eingabe einlesen
+    String s = args[0];
+    int number = Integer.parseInt(s);
 
-
-        //Zahl im Schema ausgeben
-        System.out.println();
+    if (number <= 0) {
+      System.out.println("Bitte nur positive Werte eingeben.");
+      System.exit(-1);
     }
-    
+
+    Fibonacci fibonacci = new Fibonacci();
+
+
+    System.out.println("| n |  f(n)  |");
+    System.out.println("+---+--------+");
+    System.out.println("|  0|       0|");
+    System.out.println("|  1|       1|");
+
+    //Gibt die Fibonacci-Zahl ab f(2) aus
+    for (int i=2;i<=number ;i++ ) {
+      System.out.printf("|%3d|%8d|\n",i,fibonacci.next());
+    }
+
+
+  }
 }
